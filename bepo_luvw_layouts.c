d52575d49d3394081b4787be2b2c15763b8e72f4
Luvwahraan <luvwahraan+githubs@pikaed.org>
2019-06-09 14:42:35 +0200

    fix merged layers

--- bepo_luvw_layouts.c
+++ bepo_luvw_layouts.c
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |                                  |      |------+------+------+------+------+--------|
  * |        |   a  |   u  |   i  |   e  |   ,  |------|                                  |------|   c  |   t  |   s  |   r  |   n  |   m    |
  * |--------+------+------+------+------+------| BKSP |                                  | BKSP |------+------+------+------+------+--------|
- * |        |a_grav|   y  |   x  |   .  |   k  |      |                                  |      |   '  |   q  |   g  |   h  |   f  |        |
+ * | LShift |a_grav|   y  |   x  |   .  |   k  |      |                                  |      |   '  |   q  |   g  |   h  |   f  | Rshift |
  * `--------+------+------+------+------+-------------,-------------.      ,-------------`-------------+------+------+------+------+--------'
- *   | Left |  Up  |   %  | PgUp | LDesk|             | C+alt|Super |      | Super| C+alt|             | RDesk|PgDown|   W  | Down | Right|
+ *   | Left |  Up  |   %  | PgUp | LDesk|             | C+alt|Super |      | Super| C+alt|             | RDesk|PgDown|      | Down | Right|
  *   `----------------------------------'      ,------|------|------|      |------+------+------.      `----------------------------------'
  *                                             |  FN  |      |lctrl |      |rctrl |      |  FN  |
  *                                             | space| Enter|------|      |------|Enter |space |
@@ -53,10 +53,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BEPO] = LAYOUT_ergodox(
 //BP_DOLLAR,BP_DQOT,      BP_LGIL,     BP_RGIL,     BP_LPRN,     BP_RPRN,     KC_BSPC,        // left hand
-TD_BPESC,   TD_F1,       TD_F2,       TD_F3,       TD_F4,       TD_F5,       BP_EQUAL,
+TD_BPESC,    TD_F1,       TD_F2,       TD_F3,       TD_F4,       TD_F5,       BP_EQUAL,
 KC_TAB,      BP_B,        BP_ECUT,     BP_P,        BP_O,        BP_EGRV,     KC_LEAD,
-_______,        BP_A,        BP_U,        BP_I,        BP_E,        BP_COMMA,
-_______,     BP_AGRV,     BP_Y,        BP_X,        BP_DOT,      BP_K,        KC_BSPC,
+BP_W,        BP_A,        BP_U,        BP_I,        BP_E,        BP_COMMA,
+KC_LSHIFT,   BP_AGRV,     BP_Y,        BP_X,        BP_DOT,      BP_K,        KC_BSPC,
 TD_COPY,     TD_PASTE,    BP_PERC,     KC_PGUP,     LWS,
                                                                               TD_LALT,     TD_GUI,
                                                                                            LCTLENT,
@@ -65,8 +65,8 @@ TD_COPY,     TD_PASTE,    BP_PERC,     KC_PGUP,     LWS,
                           KC_DEL,      TD_F6,       TD_F7,       TD_F8,       TD_F9,       TD_F10,      BP_EQUAL, // right hand
                           KC_LEAD,     BP_DCRC,     BP_V,        BP_D,        BP_L,        BP_J,        BP_Z,
                                        BP_C,        BP_T,        BP_S,        BP_R,        BP_N,        BP_M,
-                          KC_BSPC,     BP_APOS,     BP_Q,        BP_G,        BP_H,        BP_F,        _______,
-                                                    RWS,         KC_PGDOWN,   BP_W,        KC_DOWN,     KC_RIGHT,
+                          KC_BSPC,     BP_APOS,     BP_Q,        BP_G,        BP_H,        BP_F,        KC_RSHIFT,
+                                                    RWS,         KC_PGDOWN,   _______,     KC_DOWN,     KC_RIGHT,
 TD_GUI,     TD_RALT,
 RCTLENT,
 TD_RCTRL,   KC_ENTER,    KC_SPC),
