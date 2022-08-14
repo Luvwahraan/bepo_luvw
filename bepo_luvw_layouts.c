11e014df59ba1e1a10a61a8dfb6e7ceac3ab8719
Luvwahraan <Luvwahraan+github@pikaed.org>
2022-08-14 09:25:56 +0200

    Right layout finished.

--- bepo_luvw_layouts.c
+++ bepo_luvw_layouts.c
@@ -5,6 +5,10 @@
 #include "bepo_luvw_tap_dance.h"
 #include "bepo_luvw_layouts.h"
 #include "bepo_luvw_shortcuts.h"
+ 
+ /* ToDo
+  * Mod tap on ENTER
+  */
 
 
 
@@ -46,11 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | SPC  |   '  |   q  |   g  |   h  | f    | W    | MAJ  | *
  * |      |      |      |      |      | RALT | RAPP |      | *
  * `--------+------+------+------+------+----+------+------' */
-/*[0] = LAYOUT_ergodox(
-    KC_DEL,      TD_F6,       TD_F7,       TD_F8,       TD_F9,       TD_F10,      BP_EQUAL,     BP_PERC, // right hand
-    _______,     BP_DCRC,     BP_V,        BP_D,        BP_L,        BP_J,        BP_Z,         _______,
-    BP_C,        BP_T,        BP_S,        BP_R,        BP_N,        BP_M,        _______,      _______,
-    _______,     BP_APOS,     BP_Q,        BP_G,        BP_H,        BP_F,        BP_W,         _______
-    //RWS,         KC_PGDOWN,   OSL(FLOK),   KC_DOWN,     KC_RIGHT,    _______,     BP_W,         _______
+/*[0] = LAYOUT_ergodox( // right hand
+    KC_BSPC,     TD_F6,       TD_F7,       TD_F8,       TD_F9,       TD_F10,      BP_EQUAL,     BP_PERC,
+    _______,     BP_DCRC,     BP_V,        BP_D,        BP_L,        BP_J,        BP_Z,         KC_UNDO,
+    KC_ENTER,    BP_C,        BP_T,        BP_S,        BP_R,        BP_N,        BP_M,         KC_MUTE,
+    KC_SPC,      BP_APOS,     BP_Q,        BP_G,        BP_H,        BP_F,        BP_W,         KC_LSHIFT
 )};
 */
