a5a4de9194c6a98ae319e9b1d50223a9f68a6c45
Luvwahraan <Luvwahraan+github@pikaed.org>
2022-08-14 16:57:28 +0200

    updated layouts

--- bepo_luvw_layouts.c
+++ bepo_luvw_layouts.c
@@ -13,8 +13,8 @@
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = LAYOUT_ergodox(
-    /* keymap 0: default left layer                              *
+[0] = LAYOUT_splitreduced(
+    /* keymap 0: default left layer for Linux                    *
     /* QK_BOOT is hidden inside the case.                        *
      * ,------+------+------+------+------+------+------+------. *
      * |  $   |"    1|«    2|»    3|(    4|)    5|      |      | *
@@ -26,38 +26,38 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |PASTE | TAB  | a    | u    | i    | e    | ,    |      | *
      * |      | LCTL |      |      |      |      | PGDW | CMP  | *
      * |------+------+------+------+------+------+------|------| *
-     * |      | MAJ  | à    | y    | x    | .    | k    | SPC  | *
-     * |      |      | LCTL | LALT |      |      |      |      | *
+     * |LSHIFT|LSHIFT| à    | y    | x    | .    | k    | SPC  | *
+     * | CMP  | CMP  | LCTL | LALT |      |      |      | LWS  | *
      * `------+------+------+------+------+------+------+------' */
         TD_BPESC,   TD_F1,      TD_F2,      TD_F3,      TD_F4,      TD_F5,      KC_DEL,     KC_BSPC,  
         TD_COPY,    TD_TAB,     BP_B,       BP_ECUT,    BP_P,       BP_O,       TD_EGRV,    QK_BOOT,
         KC_PASTE,   TD_TAB,     BP_A,       BP_U,       BP_I,       BP_E,       TD_COMMA,   TD_ENTER, 
-        _______,    TD_LSHIFT,  TD_AGRV,    TD_Y,       BP_X,       BP_DOT,     BP_K,       TD_LLSPC
+        TD_LSHIFT,  TD_LSHIFT,  TD_AGRV,    TD_Y,       BP_X,       BP_DOT,     BP_K,       TD_LLSPC
     ),
-[1] = LAYOUT_ergodox(
-    /* keymap 1: right on left layer                             *
+[1] = LAYOUT_splitreduced(
+    /* keymap 1: right on left layer for windows                 *
      * ,------+------+------+------+------+------+------+------. *
-     * |@    6|+    7|-    8|/    9|*    0|=    °|%    `|      | *
-     * |F6    |F7    |F8    |F9    |F10   |F11   |F12   |      | *
+     * |      |      |      |      |      |      |      |      | *
+     * |      |      |      |      |      |      |      |      | *
      * |------+------+------+------+------+------+------|      | *
-     * | TAB  | ^    | v    | d    | l    | j    |  z   |______| *
-     * | LCTL |      |      |      |      |      |      |      | *
+     * |      |      |      |      |      |      |      |______| *
+     * |      |      |      |      |      |      |      |      | *
      * |------+------+------+------+------+------+------|      | *
-     * | TAB  | c    | t    | s    | r    | n    | m    |      | *
-     * | LCTL |      |      |      |      |      |      |      | *
+     * |      |      |      |      |      |      |      |      | *
+     * |      |      |      |      |      |      |      |      | *
      * |------+------+------+------+------+------+------|------| *
-     * | MAJ  | '    | q    | g    | h    | f    | W    |      | *
-     * |      |      |      |      |      | RALT | APP  |      | *
+     * |      |      |      |      |      |      |      | SPC  | *
+     * |      |      |      |      |      |      |      | LWS  | *
      * `------+------+------+------+------+------+------+------' */
-        TD_F6,      TD_F7,      TD_F8,      TD_F9,      TD_F10,     BP_EQUAL,   BP_PERC,    _______, 
-        TD_TAB,     BP_DCRC,    BP_V,       BP_D,       BP_L,       BP_J,       BP_Z,       _______,
-        TD_TAB,     BP_C,       BP_T,       BP_S,       BP_R,       BP_N,       BP_M,       _______, 
-        TD_LSHIFT,  BP_APOS,    BP_Q,       BP_G,       BP_H,       TD_F,       TD_W,       _______   
+        _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
+        _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
+        _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
+        _______,    _______,    _______,    _______,    _______,    _______,    _______,    TD_LWSPC   
     )
 };
 
 
-/*[0] = LAYOUT_ergodox(
+/*[0] = LAYOUT_splitreduced(
     /* keymap 0: default right layer                             *
     /* QK_BOOT is hidden inside the case.                        *
      * ,------+------+------+------+------+------+------+------. *
@@ -70,12 +70,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |      | c    | t    | s    | r    | n    | m    | MUTE | *
      * | CMP  | ç    |      |      |      |      |      | RCTL | *
      * |------+------+------+------+------+------+------+------| *
-     * | SPC  | '    | q    | g    | h    | f    | W    | MAJ  | *
-     * |      |      |      |      |      | RALT | APP  |      | *
+     * | SPC  | '    | q    | g    | h    | f    | W    |RSHIFT| *
+     * | RWS  |      |      |      |      | RALT | APP  | CMP  | *
      * `------+------+------+------+------+------+------+------' *
         KC_BSPC,    TD_F6,      TD_F7,      TD_F8,      TD_F9,      TD_F10,     BP_EQUAL,   BP_PERC,
         QK_BOOT,    BP_DCRC,    BP_V,       BP_D,       BP_L,       BP_J,       BP_Z,       TD_UNDO,
         TD_ENTER,   BP_C,       BP_T,       BP_S,       BP_R,       BP_N,       BP_M,       TD_MUTE,
         TD_RLSPC,   BP_APOS,    BP_Q,       BP_G,       BP_H,       TD_F,       TD_W,       TD_RSHIFT
+    ),
+[1] = LAYOUT_splitreduced(
+    /* keymap 0: default right layer for windows                 *
+    /* QK_BOOT is hidden inside the case.                        *
+     * ,------+------+------+------+------+------+------+------. *
+     * |      |      |      |      |      |      |      |      | *
+     * |      |      |      |      |      |      |      |      | *
+     * |      |------+------+------+------+------+------+------| *
+     * |______|      |      |      |      |      |      |      | *
+     * |      |      |      |      |      |      |      |      | *
+     * |      |------+------+------+------+------+------+------| *
+     * |      |      |      |      |      |      |      |      | *
+     * |      |      |      |      |      |      |      |      | *
+     * |------+------+------+------+------+------+------+------| *
+     * | SPC  |      |      |      |      |      |      |      | *
+     * | RWS  |      |      |      |      |      |      |      | *
+     * `------+------+------+------+------+------+------+------' *
+        _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
+        _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
+        _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
+        TD_RWSPC,   _______,    _______,    _______,    _______,    _______,    _______,    _______
     )
 };*/
