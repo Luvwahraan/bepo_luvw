91fd00df886871b5fa258a88a59f04768b533f58
Luvwahraan <Luvwahraan+github@pikaed.org>
2022-08-14 10:21:53 +0200

    Align maps

--- bepo_luvw_layouts.c
+++ bepo_luvw_layouts.c
@@ -13,47 +13,69 @@
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* keymap 0: default left layer                              *
-/* A switch is hidden inside the case: reset.                *
- * ,------+------+------+------+------+------+------+------. *
- * |  $   |"    1|«    2|»    3|(    4|)    5|      |      | *
- * |ESC   |F1    |F2    |F3    |F4    |F5    | DEL  |      | *
- * |------+------+------+------+------+------+------| BKSP | *
- * |COPY  |  TAB |   b  |   é  |   p  |   o  |   è  |______| *
- * |CUT   | LCTL |      |      |      |      |      | RET  | *
- * |------+------+------+------+------+------+------|      | *
- * |PASTE |  TAB |   a  |   u  |   i  |   e  |   ,  |      | *
- * |      | LCTL |      |      |      |      |      | CMP  | *
- * |------+------+------+------+------+------+------|------| *
- * |      |  MAJ |   à  |   y  |   x  |   .  |   k  |  SPC | *
- * |      |      |      |      |      |      |      |      | *
- * `------+------+------+------+------+------+-------------' */
-[0] = LAYOUT_ergodox( // left hand
-    TD_BPESC,    TD_F1,       TD_F2,       TD_F3,       TD_F4,       TD_F5,       KC_DEL,      KC_BSPC,  
-    TD_COPY,     KC_TAB,      BP_B,        BP_ECUT,     BP_P,        BP_O,        BP_EGRV,     _______,
-    TD_PASTE,    KC_TAB,      BP_A,        BP_U,        BP_I,        BP_E,        BP_COMMA,    KC_ENTER, 
-    _______,     KC_LSHIFT,   BP_AGRV,     BP_Y,        BP_X,        BP_DOT,      BP_K,        KC_SPC
-)};
+[0] = LAYOUT_ergodox(
+    /* keymap 0: default left layer                              *
+    /* QK_BOOT is hidden inside the case.                        *
+     * ,------+------+------+------+------+------+------+------. *
+     * |  $   |"    1|«    2|»    3|(    4|)    5|      |      | *
+     * |ESC   |F1    |F2    |F3    |F4    |F5    | DEL  |      | *
+     * |------+------+------+------+------+------+------| BKSP | *
+     * |COPY  | TAB  | b    | é    | p    | o    | è    |______| *
+     * |CUT   | LCTL |      |      |      |      |      | RET  | *
+     * |------+------+------+------+------+------+------|      | *
+     * |PASTE | TAB  | a    | u    | i    | e    | ,    |      | *
+     * |      | LCTL |      |      |      |      |      | CMP  | *
+     * |------+------+------+------+------+------+------|------| *
+     * |      | MAJ  | à    | y    | x    | .    | k    |  SPC | *
+     * |      |      | LCTL | LALT |      |      |      |      | *
+     * `------+------+------+------+------+------+------+------' */
+        TD_BPESC,   TD_F1,      TD_F2,      TD_F3,      TD_F4,      TD_F5,      KC_DEL,     KC_BSPC,  
+        TD_COPY,    KC_TAB,     BP_B,       BP_ECUT,    BP_P,       BP_O,       BP_EGRV,    QK_BOOT,
+        TD_PASTE,   KC_TAB,     BP_A,       BP_U,       BP_I,       BP_E,       BP_COMMA,   KC_ENTER, 
+        _______,    KC_LSHIFT,  BP_AGRV,    BP_Y,       BP_X,       BP_DOT,     BP_K,       KC_SPC
+    ),
+[1] = LAYOUT_ergodox(
+    /* keymap 1: right on left layer                             *
+     * ,------+------+------+------+------+------+------+------. *
+     * |@    6|+    7|-    8|/    9|*    0|=    °|%    `|      | *
+     * |F6    |F7    |F8    |F9    |F10   |F11   |F12   |      | *
+     * |------+------+------+------+------+------+------|      | *
+     * | TAB  | ^    | v    | d    | l    | j    |  z   |______| *
+     * | LCTL |      |      |      |      |      |      |      | *
+     * |------+------+------+------+------+------+------|      | *
+     * | TAB  | c    | t    | s    | r    | n    | m    |      | *
+     * | LCTL |      |      |      |      |      |      |      | *
+     * |------+------+------+------+------+------+------|------| *
+     * | MAJ  | '    | q    | g    | h    | f    | W    |      | *
+     * |      |      |      |      |      | RALT | APP  |      | *
+     * `------+------+------+------+------+------+------+------' */
+        TD_F6,      TD_F7,      TD_F8,      TD_F9,      TD_F10,     BP_EQUAL,   BP_PERC,    _______, 
+        KC_TAB,     BP_DCRC,    BP_V,       BP_D,       BP_L,       BP_J,       BP_Z,       _______,
+        KC_TAB,     BP_C,       BP_T,       BP_S,       BP_R,       BP_N,       BP_M,       _______, 
+        KC_LSHIFT,  BP_APOS,    BP_Q,       BP_G,       BP_H,       BP_F,       BP_W,       _______   
+    )
+};
 
-/* keymap 0: default right layer                             *
-/* A switch is hidden inside the case: reset.                *
- * ,------+------+------+------+------+------+------+------. *
- * |      |@    6|+    7|-    8|/    9|*    0|=    °|%    `| *
- * |      |F6    |F7    |F8    |F9    |F10   |F11   |F12   | *
- * | BKSP |------+------+------+------+------+------+------| *
- * |______|   ^  |   v  |   d  |   l  |   j  |  z   | UNDO | *
- * | RET  |      |      |      |      |      |      | REDO | *
- * |      |------+------+------+------+------+------+------| *
- * |      |   c  |   t  |   s  |   r  |   n  |  m   | MUTE | *
- * | CMP  |      |      |      |      |      |      | RCTL | *
- * |------+------+------+------+------+------+------+------| *
- * | SPC  |   '  |   q  |   g  |   h  | f    | W    | MAJ  | *
- * |      |      |      |      |      | RALT | RAPP |      | *
- * `--------+------+------+------+------+----+------+------' */
-/*[0] = LAYOUT_ergodox( // right hand
-    KC_BSPC,     TD_F6,       TD_F7,       TD_F8,       TD_F9,       TD_F10,      BP_EQUAL,     BP_PERC,
-    _______,     BP_DCRC,     BP_V,        BP_D,        BP_L,        BP_J,        BP_Z,         KC_UNDO,
-    KC_ENTER,    BP_C,        BP_T,        BP_S,        BP_R,        BP_N,        BP_M,         KC_MUTE,
-    KC_SPC,      BP_APOS,     BP_Q,        BP_G,        BP_H,        BP_F,        BP_W,         KC_LSHIFT
-)};
-*/
+
+/*[0] = LAYOUT_ergodox(
+    /* keymap 0: default right layer                             *
+    /* QK_BOOT is hidden inside the case.                        *
+     * ,------+------+------+------+------+------+------+------. *
+     * |      |@    6|+    7|-    8|/    9|*    0|=    °|%    `| *
+     * |      |F6    |F7    |F8    |F9    |F10   |F11   |F12   | *
+     * | BKSP |------+------+------+------+------+------+------| *
+     * |______| ^    | v    | d    | l    | j    |  z   | UNDO | *
+     * | RET  |      |      |      |      |      |      | EMOJI| *
+     * |      |------+------+------+------+------+------+------| *
+     * |      | c    | t    | s    | r    | n    | m    | MUTE | *
+     * | CMP  | ç    |      |      |      |      |      | RCTL | *
+     * |------+------+------+------+------+------+------+------| *
+     * | SPC  | '    | q    | g    | h    | f    | W    | MAJ  | *
+     * |      |      |      |      |      | RALT | APP  |      | *
+     * `------+------+------+------+------+------+------+------' *
+        KC_BSPC,    TD_F6,      TD_F7,      TD_F8,      TD_F9,      TD_F10,     BP_EQUAL,   BP_PERC,
+        QK_BOOT,    BP_DCRC,    BP_V,       BP_D,       BP_L,       BP_J,       BP_Z,       KC_UNDO,
+        KC_ENTER,   BP_C,       BP_T,       BP_S,       BP_R,       BP_N,       BP_M,       KC_MUTE,
+        KC_SPC,     BP_APOS,    BP_Q,       BP_G,       BP_H,       BP_F,       BP_W,       KC_LSHIFT
+    )
+};*/
