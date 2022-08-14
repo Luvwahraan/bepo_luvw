23ed77cf04dad1340c09d021e14fa85b44f25248
Luvwahraan <Luvwahraan+github@pikaed.org>
2022-08-14 14:15:35 +0200

    Added ACTION_HOLD_TAP(kc_tap, kc_hold)

--- bepo_luvw_lowline_modtap.c
+++ bepo_luvw_lowline_modtap.c
@@ -6,203 +6,42 @@
 #include "bepo_luvw_shortcuts.h"
 #include "bepo_luvw_lowline_modtap.h"
 
-// LShift
-void td_WinLeftShift_finished(qk_tap_dance_state_t *_state, void *user_data) {
-  tdState = check_tap_state(_state);
-  switch (tdState) {
-    case SINGLE_TAP_TD:
-      register_code(WLWS);
-      break;
-    case SINGLE_HOLD_TD:
-      register_code(KC_LSHIFT);
-      break;
-    case DOUBLE_TAP_TD:
-    case DOUBLE_SINGLE_TAP_TD:
-    case DOUBLE_HOLD_TD:
-    case UNKNOWN_TD:
-    default:
-      register_code(KC_LSHIFT);
-      break;
-  }
-}
-void td_WinLeftShift_reset(qk_tap_dance_state_t *_state, void *user_data) {
-    switch (tdState) {
-    case SINGLE_TAP_TD:
-        unregister_code(WLWS);
-        break;
-    case SINGLE_HOLD_TD:
-        unregister_code(KC_LSHIFT);
-        break;
-    case DOUBLE_TAP_TD:
-    case DOUBLE_SINGLE_TAP_TD:
-    case DOUBLE_HOLD_TD:
-    case UNKNOWN_TD:
-    default:
-        unregister_code(KC_LSHIFT);
-        break;
-    }
-    tdState = 0;
-}
-// end LShift
 
-// RShift
-void td_RightShift_finished(qk_tap_dance_state_t *_state, void *user_data) {
+// Generic hold tap
+
+void td_generic_finished(qk_tap_dance_state_t *_state, void *user_data) {
+    qk_tap_dance_pair_t *pair = (qk_tap_dance_pair_t *)user_data;
+
     tdState = check_tap_state(_state);
     switch (tdState) {
     case SINGLE_TAP_TD:
-        register_code(WRWS);
+        register_code(pair->kc1);
         break;
     case SINGLE_HOLD_TD:
-        register_code(KC_RSHIFT);
+        register_code(pair->kc2);
         break;
-    case DOUBLE_TAP_TD:
-    case DOUBLE_SINGLE_TAP_TD:
-    case DOUBLE_HOLD_TD:
     case UNKNOWN_TD:
     default:
-        register_code(KC_RSHIFT);
+        register_code(pair->kc2);
         break;
     }
 }
-void td_RightShift_reset(qk_tap_dance_state_t *_state, void *user_data) {
+
+void td_generic_reset(qk_tap_dance_state_t *_state, void *user_data) {
+    qk_tap_dance_pair_t *pair = (qk_tap_dance_pair_t *)user_data;
+
   switch (tdState) {
     case SINGLE_TAP_TD:
-      unregister_code(WRWS);
+      unregister_code(pair->kc1);
       break;
     case SINGLE_HOLD_TD:
-      unregister_code(KC_RSHIFT);
+      unregister_code(pair->kc2);
       break;
-    case DOUBLE_TAP_TD:
-    case DOUBLE_SINGLE_TAP_TD:
-    case DOUBLE_HOLD_TD:
     case UNKNOWN_TD:
     default:
-      unregister_code(KC_RSHIFT);
+      unregister_code(pair->kc2);
       break;
   }
   tdState = 0;
 }
-// end RShift
-
-
-// Linux versions
-
-// LShift
-void td_LinuxLeftShift_finished(qk_tap_dance_state_t *_state, void *user_data) {
-  tdState = check_tap_state(_state);
-  switch (tdState) {
-    case SINGLE_TAP_TD:
-      register_code(LWS);
-      break;
-    case SINGLE_HOLD_TD:
-      register_code(KC_LSHIFT);
-      break;
-    case DOUBLE_TAP_TD:
-    case DOUBLE_SINGLE_TAP_TD:
-    case DOUBLE_HOLD_TD:
-    case UNKNOWN_TD:
-    default:
-      register_code(KC_LSHIFT);
-      break;
-  }
-}
-void td_LinuxLeftShift_reset(qk_tap_dance_state_t *_state, void *user_data) {
-    switch (tdState) {
-    case SINGLE_TAP_TD:
-        unregister_code(LWS);
-        break;
-    case SINGLE_HOLD_TD:
-        unregister_code(KC_LSHIFT);
-        break;
-    case DOUBLE_TAP_TD:
-    case DOUBLE_SINGLE_TAP_TD:
-    case DOUBLE_HOLD_TD:
-    case UNKNOWN_TD:
-    default:
-        unregister_code(KC_LSHIFT);
-        break;
-    }
-    tdState = 0;
-}
-// end LShift
-
-// RShift
-void td_LinuxRightShift_finished(qk_tap_dance_state_t *_state, void *user_data) {
-    tdState = check_tap_state(_state);
-    switch (tdState) {
-    case SINGLE_TAP_TD:
-        register_code(RWS);
-        break;
-    case SINGLE_HOLD_TD:
-        register_code(KC_RSHIFT);
-        break;
-    case DOUBLE_TAP_TD:
-    case DOUBLE_SINGLE_TAP_TD:
-    case DOUBLE_HOLD_TD:
-    case UNKNOWN_TD:
-    default:
-        register_code(KC_RSHIFT);
-        break;
-    }
-}
-void td_LinuxRightShift_reset(qk_tap_dance_state_t *_state, void *user_data) {
-  switch (tdState) {
-    case SINGLE_TAP_TD:
-      unregister_code(RWS);
-      break;
-    case SINGLE_HOLD_TD:
-      unregister_code(KC_RSHIFT);
-      break;
-    case DOUBLE_TAP_TD:
-    case DOUBLE_SINGLE_TAP_TD:
-    case DOUBLE_HOLD_TD:
-    case UNKNOWN_TD:
-    default:
-      unregister_code(KC_RSHIFT);
-      break;
-  }
-  tdState = 0;
-}
-// end RShift
-
-
-
-// Enter
-void td_Enter_finished(qk_tap_dance_state_t *_state, void *user_data) {
-    tdState = check_tap_state(_state);
-    switch (tdState) {
-    case SINGLE_TAP_TD:
-        register_code(KC_ENTER);
-        break;
-    case SINGLE_HOLD_TD:
-        register_code(BP_COMPOSE);
-        break;
-    case DOUBLE_TAP_TD:
-    case DOUBLE_SINGLE_TAP_TD:
-    case DOUBLE_HOLD_TD:
-    case UNKNOWN_TD:
-    default:
-        register_code(BP_COMPOSE);
-        break;
-    }
-}
-void td_Enter_reset(qk_tap_dance_state_t *_state, void *user_data) {
-  switch (tdState) {
-    case SINGLE_TAP_TD:
-      unregister_code(KC_ENTER);
-      break;
-    case SINGLE_HOLD_TD:
-      unregister_code(BP_COMPOSE);
-      break;
-    case DOUBLE_TAP_TD:
-    case DOUBLE_SINGLE_TAP_TD:
-    case DOUBLE_HOLD_TD:
-    case UNKNOWN_TD:
-    default:
-      unregister_code(BP_COMPOSE);
-      break;
-  }
-  tdState = 0;
-}
-// end Enter
-
+// End generic hold tap
--- bepo_luvw_lowline_modtap.h
+++ bepo_luvw_lowline_modtap.h
@@ -1,5 +1,5 @@
-#ifndef BEPO_LUVW_TAP_DANCE
-#define BEPO_LUVW_TAP_DANCE
+#ifndef BEPO_LUVW_MODTAP
+#define BEPO_LUVW_MODTAP
 
 #include "utils.h"
 
@@ -15,5 +15,15 @@ void td_RightShift_reset(qk_tap_dance_state_t *, void *);
 void td_Enter_finished(qk_tap_dance_state_t *, void *);
 void td_Enter_reset(qk_tap_dance_state_t *, void *);
 
+// Generic hold tap
+
+// Add predefined action like quantum/process_keycode/process_tap_dance.h
+#define ACTION_HOLD_TAP(kc_tap, kc_hold) \
+    { .fn = {NULL, td_generic_finished, td_generic_reset}, .user_data = (void *)&((qk_tap_dance_pair_t){kc_tap, kc_hold}), }
+
+// ACTION_HOLD_TAP(kc_tap, kc_hold);
+
+void td_generic_finished(qk_tap_dance_state_t*, void*);
+void td_generic_reset(qk_tap_dance_state_t*, void*);
 
 #endif
